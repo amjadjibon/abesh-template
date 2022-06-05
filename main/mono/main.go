e847ed05653bb54e22f14687ba7f056e6bfe81e4
Amjad Hossain <amjad.hossain@upaybd.com>
2022-06-05 12:49:01 +0600

    template update

--- main/mono/main.go
+++ main/mono/main.go
@@ -4,10 +4,9 @@ import (
 	_ "embed"
 
 	"github.com/mkawserm/abesh/cmd"
+	_ "github.com/mkawserm/httpserver2/capability/httpserver2"
 
 	_ "github.com/amjadjibon/abesh-template/capability/health"
-
-	_ "github.com/mkawserm/httpserver2/capability/httpserver2"
 )
 
 //go:embed manifest.yaml
